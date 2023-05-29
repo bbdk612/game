7b342c44beab1c1aab13af5564d8d1c34f0f85e1
bbdk612 <smsmisha51@gmail.com>
2023-05-29 13:49:19 +0500

    created gitingore and rewrited gamemap module

--- gamemap/map.go
+++ gamemap/map.go
@@ -27,12 +27,26 @@ func (GM *GameMap) CheckDirection(direction string) (int, bool) {
 	}
 }
 
+func (GM *GameMap) GetCurrentChunk() ([]int) {
+  return GM.chunks[GM.currentChunk]
+}
+
 func (GM *GameMap) ChangeCurrentChunk(chunk int) error {
 	if chunk > len(GM.chunks) {
 		return errors.New("Chunk is out of range")
 	}
 	GM.currentChunk = chunk
 	return nil
+}
+
+func (GM *GameMap) GetTile(tileNumber int) (*ebiten.Image) {
+  w := GM.tileset.Bounds().Dx()
+  tileXCount := w / GM.tilesize
+  
+  tileStartX := (tileNumber % tileXCount) * GM.tilesize
+  tileStartY := (tileNumber / tileXCount) * GM.tilesize
+
+  return GM.tileset.SubImage(image.Rect(tileStartX, tileStartY, tileStartX+GM.tilesize, tileStartY+GM.tilesize)).(*ebiten.Image)
 }
 
 func NewGameMap(chunks [][]int, currentChunk int, roadsTo []map[string]int, sreenWidth int, sreenHeight int) (*GameMap, error) {
--- testgamemap/main.go
+++ testgamemap/main.go
@@ -1,7 +1,23 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 
-  "github.com/bbdk612/game/gamemap"
+	"github.com/bbdk612/game/gamemap"
 )
+
+const (
+  screenWidth = 240
+  screenHeight = 240
+)
+
+const (
+  tileSize = 16
+)
+
+type Game struct {
+  Map gamemap.GameMap
+  layers [][]int 
+}
+ 
+

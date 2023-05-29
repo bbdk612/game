aa1aa829342c29fecbc9aed83267c17bbd1f60cd
bbdk612 <smsmisha51@gmail.com>
2023-05-29 15:37:24 +0500

    testing modules

--- testgamemap/main.go
+++ testgamemap/main.go
@@ -3,21 +3,27 @@ package main
 import (
 	"fmt"
 
+  "github.com/hajimehoshi/ebiten/v2"
+ // "github.com/hajimehoshi/ebiten/v2/ebitenutil"
+
 	"github.com/bbdk612/game/gamemap"
-)
-
-const (
-  screenWidth = 240
-  screenHeight = 240
-)
-
-const (
-  tileSize = 16
 )
 
 type Game struct {
   Map gamemap.GameMap
   layers [][]int 
+}
+
+func (G *Game) Update() error {
+  return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+  
+}
+
+func main() {
+  fmt.Println("hello, world")
 }
  
 

b836035f6422ce07f2926e661a57ce43289300f8
Kidcreole2 <131256373+Kidcreole2@users.noreply.github.com>
2023-06-10 21:28:53 +0500

    try to run 1

--- ui/UserInterface.go
+++ ui/UserInterface.go
@@ -1,21 +1,20 @@
 package ui
 
-import (
-)
+import ()
 
 type UI struct {
 	healthBar *HeathBar
 	weaponBar *WeaponBar
 }
 
-func InitUI() (*UI,error){
-	ui.healthBar, err :=InitHealthBar(./assets/startWeapon.png)
+func InitUI() (*UI, error) {
+	ui.healthBar, err := InitHealthBar("./assets/startWeapon.png")
 	if err != nil {
 		return nil, err
 	}
-	ui.weaponBar, err :=InitWeaponBar(./assets/startWeapon.png)
+	ui.weaponBar, err := InitWeaponBar("./assets/startWeapon.png")
 	if err != nil {
 		return nil, err
 	}
-	return ui, nil;
+	return ui, nil
 }

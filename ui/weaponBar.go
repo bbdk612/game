ca80a3f9cede5b81bb776f81ba63d4da7f19dcba
bbdk612 <sircenkom239@gmail.com>
2023-06-11 19:46:43 +0500

    create font field for weaponBar

--- ui/weaponBar.go
+++ ui/weaponBar.go
@@ -1,15 +1,27 @@
 package ui
 
 import (
+	"fmt"
 	"image"
 	"os"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/opentype"
 )
 
 type WeaponBar struct {
 	startX, startY int
 	Image          *ebiten.Image
+	AmmoFont       font.Face
+}
+
+func (wb *WeaponBar) GetAmmo(currAmmo, maxAmmo int) string {
+	return fmt.Sprintf("Ammo %d/%d", currAmmo, maxAmmo)
+}
+
+func (wb *WeaponBar) GetWpbStartCoordinate() (int, int) {
+	return wb.startX, wb.startY
 }
 
 func InitWeaponBar(imagePath string) (*WeaponBar, error) {
@@ -27,10 +39,22 @@ func InitWeaponBar(imagePath string) (*WeaponBar, error) {
 
 	weaponBarImage := ebiten.NewImageFromImage(weaponBarFileDecoded)
 
+	fontBytes, err := os.ReadFile("./assets/font.ttf")
+	if err != nil {
+		return nil, err
+	}
+
+	font, err := opentype.NewFace(fontBytes, &opentype.FaceOptions{
+		Size:    14,
+		DPI:     72,
+		Hinting: font.HintingVertical,
+	})
+
 	wpB := &WeaponBar{
-		startX: 10,
-		startY: 100,
-		Image:  weaponBarImage,
+		startX:   250,
+		startY:   150,
+		Image:    weaponBarImage,
+		AmmoFont: font,
 	}
 
 	return wpB, nil

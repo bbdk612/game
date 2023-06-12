4229ca5ab3cee5dd549a0c496ef0da51fe8056d5
bbdk612 <sircenkom239@gmail.com>
2023-06-12 20:48:48 +0500

    fix this shit

---
+++ menu/mainmenu.go
@@ -0,0 +1,74 @@
+package menu
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"image"
+	"os"
+)
+
+type MainMenu struct {
+	inMainMenu     bool
+	startbuttonX   int
+	startbuttonY   int
+	startbuttonImg *ebiten.Image
+	exitbuttonX    int
+	exitbuttonY    int
+	exitbuttonImg  *ebiten.Image
+}
+
+func InitMenu(startbuttonImagePath, exitbuttonImagePath string) (*MainMenu, error) {
+	startbuttonFile, err := os.Open(startbuttonImagePath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	startbuttonFileDecoded, _, err := image.Decode(startbuttonFile)
+
+	if err != nil {
+		return nil, err
+	}
+
+	startbuttonImage := ebiten.NewImageFromImage(startbuttonFileDecoded)
+
+	exitbuttonFile, err := os.Open(exitbuttonImagePath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	exitbuttonFileDecoded, _, err := image.Decode(exitbuttonFile)
+
+	if err != nil {
+		return nil, err
+	}
+
+	exitbuttonImage := ebiten.NewImageFromImage(exitbuttonFileDecoded)
+
+	mainM := &MainMenu{
+		inMainMenu:     true,
+		startbuttonX:   10,
+		startbuttonY:   50,
+		startbuttonImg: startbuttonImage,
+		exitbuttonX:    25,
+		exitbuttonY:    75,
+		exitbuttonImg:  exitbuttonImage,
+	}
+
+	return mainM, nil
+}
+
+func (mm *MainMenu) MenuStartGame() {
+	mm.inMainMenu = false
+}
+
+func (mm *MainMenu) MenuExitGame() {
+	os.Exit(0)
+}
+func (mm *MainMenu) GetMainMStartCoordinate() (int, int, int, int) {
+	stbX := mm.startbuttonX
+	stbY := mm.startbuttonY
+	extX := mm.exitbuttonX
+	extY := mm.exitbuttonY
+	return stbX, stbY, extX, extY
+}
--- testgamemap/test.update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-while true
-do 
-	go get github.com/bbdk612/game/animatedobjects@animatedObjDev 
-	sleep 2 * 60
-done
